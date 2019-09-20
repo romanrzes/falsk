edd0e090073817c7e75581c4aefe0ebc09836d55
Maksym Kotiash <max.siemens.fan@gmail.com>
2019-09-20 10:31:54 +0300

    Added register, login and sqlite3

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, flash, render_template, request, redirect, session, url_for, g
-from flask_login import login_user, LoginManager
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Required
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, flash, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash
 import pymysql
 from tables import Results
 from flaskext.mysql import MySQL
-from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators, Form
 from passlib.hash import sha256_crypt
 import sqlite3
@@ -29,42 +25,26 @@ def main_page():
     if request.method == 'POST':
         username = request.form['username']
         password_candidate = request.form['password']
+        db_select = f"SELECT password FROM admins WHERE username = '{username}'"
+        cur = sqlite3.connect(DATABASE).cursor()
+        answer = cur.execute(db_select)
+        res = cur.fetchone()
 
-        if username in user_list.keys():
-            print(user_list[username])
-            password = user_list[username]
-            if password_candidate == password:
+        if res:
+            password = res[0]
+            if sha256_crypt.verify(password_candidate, password):
                 session['logged_in'] = True
                 session['username'] = username
                 flash('You are now logged in', 'success')
                 return redirect(url_for('dashboard'))
             else:
-                wr_pass = 'Wrong password'
-                return render_template('login.html', wr_pass=wr_pass)
+                error = 'Invalid password'
+                return render_template('login.html', error=error)
         else:
-            error = 'Invalid login'
-            print("WRONG")
+            error = 'Username not found'
             return render_template('login.html', error=error)
+
     return render_template('login.html')
-
-    # if username in users.keys():
-    #     print(users)
-
-    #     if answer:
-    #         password = res['password']
-    #         if sha256_crypt.verify(password_candidate, password):
-    #             session['logged_in'] = True
-    #             session['username'] = username
-    #             flash('You are now logged in', 'success')
-    #             return redirect(url_for('dashboard'))
-    #             # return render_template('login.html', msg="Success")
-    #         else:
-    #             error = 'Invalid login'
-    #             return render_template('login.html', error=error)
-    #         cur.close()
-    #     else:
-    #         error = 'Username not found'
-    #         return render_template('login.html', error=error)
 
 
 class RegisterForm(Form):
@@ -113,16 +93,16 @@ def dashboard():
 @app.route('/add', methods=['POST'])
 def add_user():
     try:
-        _name = request.form['inputName']
-        _email = request.form['inputEmail']
-        _password = request.form['inputPassword']
+        user_name = request.form['inputName']
+        user_email = request.form['inputEmail']
+        user_password = request.form['inputPassword']
         # validate the received values
-        if _name and _email and _password and request.method == 'POST':
+        if user_name and user_email and user_password and request.method == 'POST':
             # do not save password as a plain text
             _hashed_password = generate_password_hash(_password)
             # save edits
-            sql = "INSERT INTO tbl_user(user_name, user_email, user_password) VALUES(%s, %s, %s)"
-            data = (_name, _email, _hashed_password,)
+            sql = "INSERT INTO users(user_name, user_email, user_password) VALUES(%s, %s, %s)"
+            data = (user_name, user_email, _hashed_password,)
             conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sql, data)
@@ -143,7 +123,7 @@ def users():
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM tbl_user")
+        cursor.execute("SELECT * FROM users")
         rows = cursor.fetchall()
         table = Results(rows)
         table.border = True
@@ -160,7 +140,7 @@ def edit_view(id):
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM tbl_user WHERE user_id=%s", id)
+        cursor.execute("SELECT * FROM users WHERE user_id=%s", id)
         row = cursor.fetchone()
         if row:
             return render_template('edit.html', row=row)
@@ -186,7 +166,7 @@ def update_user():
             _hashed_password = generate_password_hash(_password)
             print(_hashed_password)
             # save edits
-            sql = "UPDATE tbl_user SET user_name=%s, user_email=%s, user_password=%s WHERE user_id=%s"
+            sql = "UPDATE users SET user_name=%s, user_email=%s, user_password=%s WHERE user_id=%s"
             data = (_name, _email, _hashed_password, _id,)
             conn = mysql.connect()
             cursor = conn.cursor()
@@ -208,7 +188,7 @@ def delete_user(id):
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM tbl_user WHERE user_id=%s", (id,))
+        cursor.execute("DELETE FROM users WHERE user_id=%s", (id,))
         conn.commit()
         flash('User deleted successfully!')
         return redirect('/')
